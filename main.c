1e8f64a89ab5d8a52c7a8b7c5035879157e6788e
elliek17 <elliekarout@gmail.com>
2026-03-27 17:27:36 +0200

    fixed main

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: ekarout <ekarout@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/06 13:32:44 by ekarout           #+#    #+#             */
-/*   Updated: 2026/03/26 21:03:13 by ekarout          ###   ########.fr       */
+/*   Updated: 2026/03/27 17:26:14 by ekarout          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,25 +21,30 @@ void	sigint_handler(int sig)
 	rl_redisplay();
 }
 
-int	main(int argc, char **argv, char **envp)
+void	read_input(char	*input, t_vars *vars, t_gc **garbage_collector)
 {
-	char	*input;
 	t_token	*tokens;
 	t_ast	*abstract_syntax_tree;
-	t_gc	*garbage_collector;
-	t_gc	*p_garbage_collector;
-	t_vars	vars;
 
-	signal(SIGINT, sigint_handler);
-    rl_catch_signals = 0;
-	p_garbage_collector = NULL;
-	vars_init(&vars, envp);
-	if (!argc || !argv)
-		return (0);
+	tokens = NULL;
+	tokens = tokenize(input, garbage_collector);
+	if (!tokens)
+		return ;
+	param_expand(&tokens, *vars, garbage_collector);
+	// print_tokens(tokens);
+	abstract_syntax_tree = parse(tokens, garbage_collector);
+	// print_tree(abstract_syntax_tree);
+	execute_ast(abstract_syntax_tree, vars, garbage_collector);
+}
+
+void	run_shell(t_vars *vars)
+{
+	char	*input;
+	t_gc	*garbage_collector;
+
 	while (1)
 	{
 		garbage_collector = NULL;
-		tokens = NULL;
 		input = readline("minishell$ ");
 		if (!input)
 			break ;
@@ -47,22 +52,24 @@ int	main(int argc, char **argv, char **envp)
 			add_history(input);
 		if (!*input)
 			continue ;
-		tokens = tokenize(input, &garbage_collector);
-		if (!tokens)
-		{
-			free_garbage(&garbage_collector);
-			free(input);
-			continue ;
-		}
-		param_expand(&tokens, vars, &garbage_collector);
-			// Here will be the expansion of the tokens one by one
-		// print_tokens(tokens);
-		abstract_syntax_tree = parse(tokens, &garbage_collector);
-		// print_tree(abstract_syntax_tree);
-		execute_ast(abstract_syntax_tree, &vars, &garbage_collector);
+		read_input(input, vars, &garbage_collector);
 		free_garbage(&garbage_collector);
 		free(input);
 	}
+}
+
+int	main(int argc, char **argv, char **envp)
+{
+	t_gc	*p_garbage_collector;
+	t_vars	vars;
+
+	if (!argc || !argv)
+		return (0);
+	signal(SIGINT, sigint_handler);
+    rl_catch_signals = 0;
+	p_garbage_collector = NULL;
+	vars_init(&vars, envp);
+	run_shell(&vars);
 	free_garbage(&p_garbage_collector);
 	env_clear(vars.env);
 	env_clear(vars.exp);
