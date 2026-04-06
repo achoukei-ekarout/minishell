ee89168f51b971d55814d1f7de82ec845c2c3e8a
elliek17 <elliekarout@gmail.com>
2026-04-06 04:55:55 +0300

    fixed quotes

--- src/parse/token_get_index.c
+++ src/parse/token_get_index.c
@@ -6,28 +6,58 @@
 /*   By: ekarout <ekarout@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/28 23:31:36 by achoukei          #+#    #+#             */
-/*   Updated: 2026/04/06 00:27:31 by ekarout          ###   ########.fr       */
+/*   Updated: 2026/04/06 04:52:15 by ekarout          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+// int	get_quote_index(char *line, int *i, t_vars *vars)
+// {
+// 	int	start;
+
+// 	start = (*i)++;
+// 	while (line[*i] && line[*i] != line[start])
+// 	{
+// 		(*i)++;
+// 		if (!line[*i])
+// 		{
+// 			quotes_error(*vars);
+// 			return (-1);
+// 		}
+// 	}
+// 	while (line[*i] && line[*i] != ' ')
+// 		(*i)++;
+// 	return (start);
+// }
+
 int	get_quote_index(char *line, int *i, t_vars *vars)
 {
 	int	start;
+	int	quote;
 
-	start = (*i)++;
-	while (line[*i] && line[*i] != line[start])
+	start = *i;
+	quote = *i;
+	while (line[*i] && !ft_isspace(line[*i]))
 	{
 		(*i)++;
+		while (line[*i] && line[*i] != line[quote])
+			(*i)++;
 		if (!line[*i])
 		{
 			quotes_error(*vars);
 			return (-1);
 		}
+		if (line[*i])
+			(*i)++;
+		if (is_quote(line[*i]))
+		{
+			quote = *i;
+			continue ;
+		}
+		while (line[*i] && !ft_isspace(line[*i]))
+			(*i)++;
 	}
-	while (line[*i] && line[*i] != ' ')
-		(*i)++;
 	return (start);
 }
 
